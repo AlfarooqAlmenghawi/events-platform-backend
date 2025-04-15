3be676eced90fb5288434bf83954682807524635
Alfarooq Almenghawi <a.almenghawi123@gmail.com>
2025-04-15 16:00:12 +0100

    Enable cors

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: "*",
+    credentials: true, // include this if you're using cookies or authorization headers
+  })
+);
 
 const ENV = process.env.NODE_ENV || "development";
 require("dotenv").config({
