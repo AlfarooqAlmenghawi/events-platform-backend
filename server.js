b8e6165b9874c0a84c17c953409dfd468a34d003
Alfarooq Almenghawi <a.almenghawi123@gmail.com>
2025-05-03 14:32:21 +0100

    Allow any frontend origin

--- server.js
+++ server.js
@@ -2,11 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-);
+app.use(cors({ origin: "*" }));
 
 const ENV = process.env.NODE_ENV || "development";
 require("dotenv").config({
